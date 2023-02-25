0631a5413f60b9b6b7dc57476f8b4e07f6f97eeb
tmwiso <tmchimbindi@wisoai.com>
2023-02-25 15:00:12 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Tafadzwa Chimbindi on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
